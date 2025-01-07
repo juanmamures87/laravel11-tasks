c163cb0be8c7c3784a3a4bc1c158f89b8e501ecf
Juanma Castro <juanmamures@gmail.com>
2025-01-08 00:58:45 +0100

    Add dashboard controller and view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,15 +8,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-3xl">BIENVENIDOS AL GESTOR DE TAREAS</h1>
-                    <h2>{{ Auth::user()->name ?? '' }} con el id {{ Auth::user()->id ?? '' }} con
-                        {{ Auth::user()->tasks->count() }} tarea/s registrada/s</h2>
-                    <h2 class="text-2xl">Nombre de la tarea:</h2>
-                    {{ Auth::user()->tasks->find(1)->title }}
-                    <h2 class="text-2xl">Contenido de la tarea:</h2>
-                    {{ Auth::user()->tasks->find(1)->description }}
-                </div>
+
+                <h1 class="text-3xl">BIENVENIDOS AL GESTOR DE TAREAS</h1>
+                <h2>{{ Auth::user()->name ?? '' }} con el id {{ Auth::user()->id ?? '' }} con
+                    {{ Auth::user()->tasks->count() }} tarea/s registrada/s</h2>
+
+                @foreach ($tasks as $task)
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <h2 class="text-2xl">Nombre de la tarea:</h2>
+                        <p>{{ $task->title }}</p>
+                        <h2 class="text-2xl">Contenido de la tarea:</h2>
+                        <p>{{ $task->description }}</p>
+                    </div>
+                @endforeach
+
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 //Route::view('/', 'welcome');
 
-Route::view('/', 'dashboard')
+Route::redirect('/', 'dashboard');
+
+Route::get('dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
